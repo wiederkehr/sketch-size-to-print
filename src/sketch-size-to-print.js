3131113bad2fd339fce4f0d7e26a76df38496587
Benjamin Wiederkehr <Benjamin@Calliope.fios-router.home>
2020-12-09 12:44:39 -0500

    Trivial implementation of copy and resize

--- src/sketch-size-to-print.js
+++ src/sketch-size-to-print.js
@@ -1,6 +1,17 @@
 import sketch from "sketch";
-// documentation: https://developer.sketchapp.com/reference/api/
 
 export default function () {
-	sketch.UI.message("It's alive 🚀");
+  const document = sketch.fromNative(context.document);
+  const selection = document.selectedLayers;
+  selection.map((layer) => {
+    const PXtoMM = 2.833333;
+    var frame = layer.frame;
+    var copy = layer.duplicate();
+    copy.frame.x = frame.x + frame.width + 100;
+    copy.frame.width = frame.width * PXtoMM;
+    copy.frame.height = frame.height * PXtoMM;
+    copy.name = layer.name + "_print";
+    selection.clear();
+    copy.selected = true;
+  });
 }
